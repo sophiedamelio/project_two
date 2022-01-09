e621df1f703ddb9897a0151ea2f649f2eb5319df
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-01-09 15:30:14 -0800

    Add nav bar to lists/index.ejs and show.ejs. Show.ejs is not currently accessing the stylesheet.

--- routes/lists.js
+++ routes/lists.js
@@ -5,11 +5,16 @@ const listCtrl = require("../controllers/lists");
 
 router.get("/lists", listCtrl.index);
 
-router.post("/lists", listCtrl.create);
+router.post("/lists", isLoggedIn, listCtrl.create);
 // show page
 router.get("/lists/:id", listCtrl.show);
 router.post("/lists/:id", listCtrl.create);
 
 router.delete("/lists/:id", listCtrl.delete);
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/auth/google");
+}
 
 module.exports = router;
