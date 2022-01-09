7716c46cb28bda11fce6488309eabde2e458cd01
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-01-09 14:25:51 -0800

    Work on delete functionality, about to change .ejs to mimic past assignment for different approach.

--- controllers/lists.js
+++ controllers/lists.js
@@ -42,10 +42,12 @@ function createList(req, res, next) {
 
 function deleteList(req, res, next) {
   console.log(req.params.id);
-  if (!req.params.id) return; // (if the req.params.id does not exist, exit function)
+  // if (!req.params.id) return; // (if the req.params.id does not exist, exit function)
   List.findOneAndDelete(req.params.id, function (err, list) {
     res.redirect(err, "/lists", {
       list,
     });
   });
+  // List.deleteOne(req.params.id);
+  // res.redirect("/lists");
 }
--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(methodOverride("_method"));
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(logger("dev"));
 app.use(express.json());
