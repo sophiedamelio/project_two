a8c856fe1e0f03305a5c6139effdb7aa84ab2bfe
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-01-24 21:04:06 -0800

    Routing issue still with adding and deleting items on lists (lists/show)

--- routes/items.js
+++ routes/items.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const itemsCtrl = require("../controllers/items");
 
 router.post("/:listId", isLoggedIn, itemsCtrl.create);
-router.delete("/:listId/items/:itemId", isLoggedIn, itemsCtrl.delete);
+router.delete("/:listId", isLoggedIn, itemsCtrl.delete);
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
